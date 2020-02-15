85fbf2727f8da24ae6855f1586315c991c6e654a
Oladele Joshua <jurshsmith@gmail.com>
2020-02-15 20:12:17 +0100

    solved capitalize each letter

--- mce/capitalize/index.js
+++ mce/capitalize/index.js
@@ -6,9 +6,30 @@
 //   capitalize('a short sentence') --> 'A Short Sentence'
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
-new String("ds").sli
+
+
+
+// function capitalize(str) {
+//     return str.split(" ").map(eachWord => eachWord.slice(0, 1).toUpperCase() +  eachWord.slice(1, eachWord.length)).join(" ");
+// }
+
+// other solution
 function capitalize(str) {
-    return str.split(" ").map(eachWord => eachWord.slice(0, 1).toUpperCase() +  eachWord.slice(1, eachWord.length)).join(" ");
+    let results = "";
+    const newStr = str.trim(); 
+
+    // lesson here the x in the loop is the index in string
+    for (x in newStr){
+        if (x == 0){
+            results += newStr[x].toUpperCase();
+        }
+        else if ((str[x - 1] === " "  && str[x] !== " ")){
+            results += str[x].toUpperCase();
+        } else {
+            results += str[x];
+        }
+    }
+    return results;
 }
 
 module.exports = capitalize;
